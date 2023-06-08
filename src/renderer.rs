6258f2c86cbc8980f2cb313458d71d1af5f52171
Vinicius <vinifreitas.d.a@gmail.com>
2023-06-08 01:04:26 -0300

    chore: changed clear color to black

--- src/renderer.rs
+++ src/renderer.rs
@@ -190,12 +190,7 @@ impl Renderer {
                 view: &view,
                 resolve_target: None,
                 ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(wgpu::Color {
-                        r: 0.1,
-                        g: 0.2,
-                        b: 0.3,
-                        a: 1.0,
-                    }),
+                    load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
                     store: true,
                 },
             })],
