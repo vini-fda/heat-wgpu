64dc6cd85c88e2e87fbd388c0b0216445f98a2d0
Vinicius <vinifreitas.d.a@gmail.com>
2023-07-02 20:35:26 -0300

    chore: removed double reference

--- src/conjugate_gradient.rs
+++ src/conjugate_gradient.rs
@@ -114,7 +114,7 @@ impl CG {
             stage.add_to_pass(&mut compute_pass);
         }
         drop(compute_pass);
-        encoder.copy_buffer_to_buffer(&r, 0, &p, 0, r.size());
+        encoder.copy_buffer_to_buffer(r, 0, p, 0, r.size());
         queue.submit(Some(encoder.finish()));
         // describes all the stages in a single iteration of the CG algorithm
         for _ in 0..self.max_steps {
