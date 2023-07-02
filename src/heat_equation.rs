a7c88a35a50061ddd3f3f038e8563612a1cc6aa0
Vinicius <vinifreitas.d.a@gmail.com>
2023-07-02 01:04:58 -0300

    feat: added b_matrix

--- src/heat_equation.rs
+++ src/heat_equation.rs
@@ -56,4 +56,44 @@ impl HeatEquation {
             &offsets,
         )
     }
+
+    /// Same as `a_matrix`, but gamma has a negative sign
+    pub fn b_matrix(&self, device: &wgpu::Device) -> DIAMatrixDescriptor {
+        let &Self { alpha, n, dt } = self;
+
+        let m = n * n;
+        let num_cols = m;
+        let num_rows = m;
+        let num_diags = 5;
+        let h = 1.0 / (n as f32);
+        let gamma: f32 = -alpha * dt / (2.0 * h * h);
+        // describes discretization matrix A
+        // obtained from discretizing U(x,y) in row-major order
+        let f = |i: usize, j: usize| -> f32 {
+            if i == j {
+                1.0 + 4.0 * gamma
+            } else if (i == j + 1 && i % n != 0)
+                || (j == i + 1 && j % n != 0)
+                || (i == j + n || j == i + n)
+            {
+                -gamma
+            } else {
+                0.0
+            }
+        };
+        let n = n as i32;
+        let offsets = vec![-n, -1, 0, 1, n];
+        let mut data: Vec<f32> = Vec::with_capacity(num_diags * m);
+        for offset in offsets.iter() {
+            for i in 0..m {
+                let j = i as i32 + offset;
+                if j >= 0 && j < m as i32 {
+                    data.push(f(i, j as usize));
+                } else {
+                    data.push(0.0);
+                }
+            }
+        }
+        DIAMatrixDescriptor::new(device, num_cols as u32, num_rows as u32, num_diags as u32, &data, &offsets)
+    }
 }
